981acf90d0827fd7217fe6d5e4a2b0e6392e8222
Soumyajoti Sen <110815098+iam9soumya@users.noreply.github.com>
2024-02-03 14:16:04 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,20 @@
+let result = document.getElementById("result");
+
+function appendToResult(value) {
+    result.value += value;
+}
+
+function clearResult() {
+    result.value = "";
+}
+function backspace(){
+    result.value = result.value.slice(0, -1);
+}
+
+function calculateResult() {
+    try {
+        result.value = eval(result.value);
+    } catch (error) {
+        result.value = "Error";
+    }
+}
